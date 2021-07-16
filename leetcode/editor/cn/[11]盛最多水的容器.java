5ad8742563aa75fbe5b95ebc57b08191e115b9e4
Jason <592623528@qq.com>
2021-07-16 16:43:21 +0800

    盛水最多的容器：双指针

--- leetcode/editor/cn/[11]盛最多水的容器.java
+++ leetcode/editor/cn/[11]盛最多水的容器.java
@@ -51,7 +51,19 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maxArea(int[] height) {
-
+        int p1 = 0;
+        int p2 = height.length - 1;
+        int p = 0;
+        while(p1 != p2) {
+           if(height[p1] > height[p2]) {
+               p = Math.max(p, height[p2] * (p2 - p1));
+               p2--;
+           } else {
+               p = Math.max(p, height[p1] * (p2 - p1));
+               p1++;
+           }
+        }
+        return p;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
