0369e25f1edc9b67acc19bfb7a3802c28b39911a
Jason <592623528@qq.com>
2021-08-11 00:38:47 +0800

    希尔排序

--- main/pers/jason/sorting/insertionsort/ShellSorter.java
+++ main/pers/jason/sorting/insertionsort/ShellSorter.java
@@ -6,25 +6,56 @@ public class ShellSorter implements Sorter {
 
   @Override
   public int[] sort(int[] array) {
-    int len = array.length;
-    int[] incre = hillIncremental(len);
-    for(int i=0;i<incre.length;i++) {
-      System.out.print(incre[i] + " ");
+    int[] increArray = hibbardHillIncremental(array.length);
+    for(int i=0;i<increArray.length;i++) {
+      int incre = increArray[i];
+      for(int j=0;j<incre;j++) {
+        insertSort(array, j, incre);
+      }
     }
-    return new int[0];
+    return array;
+  }
+
+  private void insertSort(int[] array, int begin, int incre) {
+    for(int i=begin+incre;i<array.length;i+=incre) {
+      int j;
+      for(j=begin;j<i;j+=incre) {
+        if(array[j] > array[i]) {
+          break;
+        }
+      }
+      int p = array[i];
+      for(int z=i;z>j;z-=incre) {
+        array[z] = array[z-incre];
+      }
+      array[j] = p;
+    }
   }
 
   /**
+   * hibbard增量序列 (2^k)-1
+   * 最坏时间复杂度为O(N(3/2))；平均时间复杂度约为O(N^(5/4))
    * 获取希尔增量序列，需要互为质数，且最小必须是1
    * @return
    */
-  public int[] hillIncremental(int len) {
+  private int[] hibbardHillIncremental(int len) {
     int max = (len == 1) ? 1: (int) Math.floor(Math.log((len/2)+1)/Math.log(2));
     int[] array = new int[max];
     for(int i=max;i>=1;i--) {
       array[max-i]=(2<<(i-1)) - 1;
     }
     return array;
+  }
+
+  private int[] simpleHillIncremental(int len) {
+    return new int[]{7,5,3,1};
+  }
+
+  /**
+   * Sedgewick增量序列
+   */
+  private int[] sedgewickHillIncremental(int len) {
+    return null;
   }
 
 }
